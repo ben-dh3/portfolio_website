58216a9533d4ded0152ece41969dba88e570b09f
ben-dh3 <benjaminduffield1997@gmail.com>
2024-07-20 12:00:38 +0100

    create browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import LandingPage from "./pages/landingPage";
 import PortfolioPage from "./pages/portfolioPage";
+import Recipeasy from "./pages/ProjectDetailViews/Recipeasy";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />
+  },
+  {
+    path: '/portfolio',
+    element: <PortfolioPage />
+  },
+  {
+    path: '/recipeasy',
+    element: <Recipeasy />
+  }
+]);
 
 function App() {
-  return (
-    <div>
-      <PortfolioPage />
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/pages/portfolioPage.tsx
+++ src/pages/portfolioPage.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import ProjectView from "../components/ProjectView";
 
 export default function PortfolioPage(){
 
     return(
         <div className="bg-black flex flex-col min-h-screen w-full">
-            <ProjectView image="/Recipeasy.png" title="Recipeasy" description="testing"/>
+            <Link to="/recipeasy">
+                <ProjectView image="/Recipeasy.png" title="Recipeasy" description="testing"/>
+            </Link>
 
         </div>
     )
