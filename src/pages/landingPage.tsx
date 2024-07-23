2c71e679282fb6de6aa3b4af6c63cc08e34ac82f
ben-dh3 <benjaminduffield1997@gmail.com>
2024-07-23 11:48:30 +0100

    fix model

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import PortfolioButton from "../components/PortfolioButton";
