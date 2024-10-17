cb95db7d68c180904412328bcef93a24ddac1c6a
ben-dh3 <benjaminduffield1997@gmail.com>
2024-10-17 10:56:46 +0100

    add flashtalking description

--- src/pages/ProjectDetailViews/Flashtalking.tsx
+++ src/pages/ProjectDetailViews/Flashtalking.tsx
@@ -13,7 +13,6 @@ const Flashtalking: React.FC = () => {
     const images = [
         '/projects/flashtalking/frame 1 300x250.webp',
         '/projects/flashtalking/frame 2 300x250.webp',
-        '/projects/flashtalking/frame 3 300x250.png',
     ];
 
     const { pathname } = useLocation();
@@ -27,7 +26,14 @@ const Flashtalking: React.FC = () => {
             <ProjectDetailNavigation leftArrow="/baketoday" rightArrow="/pokedex" />
             <ProjectDetailView 
                 title="Flashtalking Banner Ad Design" 
-                about="" 
+                about="A suite of 3 frame ads for Flashtalking by Mediaocean in the sizes 300x250, 300x600, 970x250, 320x50 (mobile). View the full suite of ads via the globe icon below.
+
+My goal was to use brand colours and fonts along with simple playful animation to draw attention to important messaging in the ad.
+
+Following the Flashtalking style guide, I created a range of designs in Figma by arranging the images and design components provided. For my animated component I designed a wavy shape to highlight key words in the ad, while referencing the Mediaocean brand (that uses wave imagery as a metaphor for movement and innovation).
+
+I assembled my designs with HTML and CSS in CodePen, matching the layout of my figma designs. I imported my wavy shape as an SVG from Figma and used the DrawSVG plugin with GSAP to animate an SVG mask - one that reveals the shape and one that hides it - to create a playful handwriting effect. I used a GSAP timeline to order the timing of the animations.
+" 
                 images={images} 
             />
             <div className='font-Arimo my-20 flex justify-center'>
