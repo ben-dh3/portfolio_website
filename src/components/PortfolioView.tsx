d0e5c837684c79de55cf77bd4758ed82dd709d10
ben-dh3 <benjaminduffield1997@gmail.com>
2024-11-05 14:39:54 +0000

    add rebrush project

--- src/components/PortfolioView.tsx
+++ src/components/PortfolioView.tsx
@@ -50,6 +50,9 @@ const PortfolioView = () => {
         
       </div>
       <div className='flex-1 grid grid-cols-1'>
+        <Link to="/rebrush" className="m-8 project">
+          <ProjectView image="/projects/rebrush/Home.webp" title="REBRUSH Wordpress Website" description="Wordpress, WooCommerce, Custome Themes & Plugins, Blender, Adobe Creative Suite" />
+        </Link>
         <Link to="/baketoday" className="m-8 project">
           <ProjectView image="/projects/baketoday/Pancakes and Syrup.webp" title="Bake Today Design Brief" description="Blender, Adobe Creative Suite" />
         </Link>
---
+++ src/pages/ProjectDetailViews/Rebrush.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import PortfolioView from "../../components/PortfolioView";
+import ProjectDetailNavigation from "../../components/ProjectDetailNavigation";
+import ProjectDetailView from "../../components/ProjectDetailView";
+import PageBreak from '../../components/PageBreak';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+
+const Rebrush: React.FC = () => {
+    const images = [
+        '/projects/rebrush/Home.webp',
+    ];
+
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return(
+        <div className="bg-black text-white">
+            <ProjectDetailNavigation leftArrow="/wilde" rightArrow="/pokedex" />
+            <ProjectDetailView 
+                title="REBRUSH Wordpress Website" 
+                about="" 
+                images={images} 
+            />
+            <div className='font-Arimo my-20 flex justify-center'>
+                Source Links:
+                <a href="https://rebrushhealth.wpcomstaging.com/" target='_blank' aria-label="Website Link">
+                    <FontAwesomeIcon icon={faGlobe} className="text-2xl mx-2" />
+                </a>
+                <a href="https://www.instagram.com/ben.d_h/" target='_blank' aria-label="Instagram Link">
+                    <FontAwesomeIcon icon={faInstagram} className="text-2xl mx-2" />
+                </a>
+            </div>
+            <PageBreak />
+            <PortfolioView />
+        </div>
+    )
+}
+
+export default Rebrush;
